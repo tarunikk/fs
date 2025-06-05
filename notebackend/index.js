605c8276292f61c50c2c979bd476d0dedc89a726
tksnikka <taru.k.s.nikkanen@student.jyu.fi>
2025-06-05 12:15:25 +0300

    uibuild

--- notebackend/index.js
+++ notebackend/index.js
@@ -27,12 +27,9 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-app.use(express.json()) 
-// ottaa json parserin käyttöön, päästään käsiksi HTTP-pyynnön mukana lähetettyyn dataan
-app.use(requestLogger)  
-// middleware, joka tulostaa konsoliin palvelimelle tulevien pyyntöjen perustietoja
+app.use(requestLogger)
 app.use(express.static('dist'))
-// static middleware saa expressin näyttämään staattista sisältöä eli sivun index.html ja sen lataaman JavaScriptin
+app.use(express.json())
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -40,8 +37,7 @@ app.get('/', (request, response) => {
 
 app.get('/api/notes', (request, response) => {
   response.json(notes)
-})  // näytetään kaikki notes
-
+})
 
 app.get('/api/notes/:id', (request, response) => {
   const id = request.params.id
@@ -52,48 +48,46 @@ app.get('/api/notes/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
-})    // näytetään tietyn id:n note
+})
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
-})   // poistetaan tietyn id:n note
+const generateId = () => {
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0
+  return String(maxId + 1)
+}
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
     return response.status(400).json({
-      error: 'content missing'  
-    })   // varmistetaan että noten sisältö, body.content, ei puutu
+      error: 'content missing',
+    })
   }
 
   const note = {
     content: body.content,
-    important: body.important || false, // asetetaan noten importance falseksi oletuksena
+    important: body.important || false,
     id: generateId(),
   }
-  console.log(note)
 
   notes = notes.concat(note)
+
   response.json(note)
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}   // luodaan uudelle notelle id
+app.delete('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  notes = notes.filter((note) => note.id !== id)
+
+  response.status(204).end()
+})
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
-app.use(unknownEndpoint) // ottaa käyttöön middlewaren, jolla saadaan routejen 
-// käsittelemättömistä virhetilanteista JSON-muotoinen virheilmoitus
+app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {

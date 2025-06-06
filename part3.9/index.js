48ba91b29202e523e939b19efc6710596530fa1b
tksnikka <taru.k.s.nikkanen@student.jyu.fi>
2025-06-06 13:29:23 +0300

    uibuild

--- part3.9/index.js
+++ part3.9/index.js
@@ -24,7 +24,7 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then(person => {
       if (person) {
@@ -47,7 +47,7 @@ app.get('/info', (request, response) => {
 }) */
 
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then(result => {
       response.status(204).end()
@@ -55,7 +55,7 @@ app.delete('/api/persons/:id', (request, response) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (!body.name) {

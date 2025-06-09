9bc3c8fb99df90db56f7a65a3f514cd707718d76
tksnikka <taru.k.s.nikkanen@student.jyu.fi>
2025-06-09 19:07:29 +0300

    uibuild

--- notebackend/eslint.config.mjs
+++ notebackend/eslint.config.mjs
@@ -16,7 +16,7 @@ export default [
     },    
     rules: {       
       '@stylistic/js/indent': ['error', 2],
-      // '@stylistic/js/linebreak-style': ['error', 'unix'],     
+      '@stylistic/js/linebreak-style': ['error', 'unix'],     
       '@stylistic/js/quotes': ['error', 'single'],      
       '@stylistic/js/semi': ['error', 'never'],  
       eqeqeq: 'error',  

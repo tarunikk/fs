6a2172248ada20ad75a39b3c9efc7308804a837c
Taru <tarunikkanen@gmail.com>
2026-02-04 15:58:24 +0200

    5c

--- noteApp/part2test/src/components/Note.jsx
+++ noteApp/part2test/src/components/Note.jsx
@@ -5,7 +5,7 @@ const Note = ({ note, toggleImportance }) => {
 
   return (
     <li className="note">
-      Your awesome note: {note.content}
+      {note.content}
       <button onClick={toggleImportance}>{label}</button>
     </li>
   )
--- noteApp/part2test/src/components/Note.test.jsx
+++ noteApp/part2test/src/components/Note.test.jsx
@@ -2,6 +2,21 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Note from './Note'
 
+test('renders content', () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true
+  }
+
+  render(<Note note={note} />)
+
+  const element = screen.getByText(
+    'Component testing is done with react-testing-library'
+  )
+
+  expect(element).toBeDefined()
+})
+
 test('clicking the button calls event handler once', async () => {
   const note = {
     content: 'Component testing is done with react-testing-library',

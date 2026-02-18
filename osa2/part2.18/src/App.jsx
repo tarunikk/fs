1282a5adcc53bce1c0f78ec3cc7b852488d41871
Taru <tarunikkanen@gmail.com>
2026-02-18 16:03:37 +0200

    2.18

--- osa2/part2.18/src/App.jsx
+++ osa2/part2.18/src/App.jsx
@@ -1,63 +1,88 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
-
-const Countries = ({ countriesToShow }) => {
-    return (
-      <ul>
-        {countriesToShow.map(country => 
-          <Country
-            name={country.name} />
-        )}
-      </ul> 
-    )
-  }
-
-const Country = ({ info }) => {
-return (
-    <li>
-        {info.name}
-    </li>
-)
-}  
+// import axios from 'axios'
+import countryService from './services/countries'
 
 const App = () => {
     const [countries, setCountries] = useState ([])
-    const [info, setInfo] = useState('')
+    // const [info, setInfo] = useState('')
     const [newFilter, setNewFilter] = useState('')
-    const [country, setCountry] = useState(null)
+    // const [country, setCountry] = useState(null)
 
     useEffect(() => {
-      console.log('effect run, country is now', country)
-  
-      // skip if country is not defined
-      if (country) {
-        console.log('fetching info on country')
-        axios
-          .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${country}`)
-          .then(response => {
-            console.log(response.data)
-            setInfo(response.data)
-          })
+      // console.log('effect run, country is now', country)
+      countryService
+        .getAll()
+        .then(response => {
+        console.log('promise fulfilled')
+        console.log('country names: ', response.data.map(country => country.name.common))
+        setCountries(response.data.map(country => country.name.common))
+      })
+    }, [])
+
+    const Countries = ({ countriesToShow }) => {
+      console.log(countriesToShow)
+      if (countriesToShow.length > 10) {
+        return (
+          <div>
+            <p>Too many matchws, specify another filter</p>
+          </div>
+        )
       }
-    }, [country])
+        return (
+          <ul>
+            {countriesToShow.map(country => 
+              <li>{country}</li>
+            )}
+          </ul> 
+        )
+      }
 
-    const handleChange = (event) => {
-        event.preventDefault()
-        setNewFilter(event.target.newFilter)
-    }
-
+    /**
+    const Country = ({ info }) => {
+        // skip if country is not defined
+        if (country) {
+          console.log('fetching info on country')
+          axios
+            .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${country}`)
+            .then(response => {
+              console.log(response.data)
+              setInfo(response.data)
+            })
+        }
+      return (
+          <li>
+              {info.name}
+          </li>
+      )
+    }  
+    
     const onSearch = (event) => {
         event.preventDefault()
         setCountry(value)
-    }
+    }  */
 
+    const Filter = ({ newFilter, handleFilterChange }) => {
+      return (
+        <div>
+          filter shown with 
+            <input value={newFilter} onChange={handleFilterChange} />
+        </div>
+      )
+    } 
     const countriesToShow = newFilter 
-    ? countries.filter(country => country.name.toLowerCase().includes(newFilter.toLowerCase()) === true)
-    : countries
+      ? countries.filter(country => country.toLowerCase().includes(newFilter.toLowerCase()) === true)
+      : countries
+
+    const handleFilterChange = (event) => {
+        console.log(event.target.value)
+        setNewFilter(event.target.value)
+    }
 
     return (
     <div>
-        <Filter newFilter={newFilter} handleChange={handleChange} />
+      <div>
+        <Filter newFilter={newFilter} handleFilterChange={handleFilterChange} />
+      </div>
         <Countries countriesToShow={countriesToShow} />
     </div>
     )

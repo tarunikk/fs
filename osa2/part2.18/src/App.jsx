7489802b26db927c3551c944bb180302e37c837f
Taru <tarunikkanen@gmail.com>
2026-02-23 17:01:15 +0200

    countries

--- osa2/part2.18/src/App.jsx
+++ osa2/part2.18/src/App.jsx
@@ -1,91 +1,98 @@
 import { useState, useEffect } from 'react'
-// import axios from 'axios'
+import axios from 'axios'
 import countryService from './services/countries'
 
 const App = () => {
-    const [countries, setCountries] = useState ([])
-    // const [info, setInfo] = useState('')
-    const [newFilter, setNewFilter] = useState('')
-    // const [country, setCountry] = useState(null)
+  const [countries, setCountries] = useState ([])
+  const [info, setInfo] = useState([])
+  const [value, setValue] = useState('')
+  const [country, setCountry] = useState(null)
+  
+  useEffect(() => {
+    // console.log('effect run, country is now', country)
+    countryService
+      .getAll()
+      .then(response => {
+      console.log('promise fulfilled')
+      setCountries(response.data.map(country => country.name.common))
+    })
+  }, [])
 
-    useEffect(() => {
-      // console.log('effect run, country is now', country)
-      countryService
-        .getAll()
+
+  useEffect(() => {
+    // console.log('effect run, country is now', country)
+    if (country){
+      axios
+        .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${country}`)
         .then(response => {
         console.log('promise fulfilled')
-        console.log('country names: ', response.data.map(country => country.name.common))
-        setCountries(response.data.map(country => country.name.common))
-      })
-    }, [])
+        console.log('country selected: ', response.data.name.common)
+        setInfo(response.data)
+        console.log('country info: ', JSON.stringify(response.data))
+    })
+  }
+  }, [country])
 
-    const Countries = ({ countriesToShow }) => {
-      console.log(countriesToShow)
-      if (countriesToShow.length > 10) {
-        return (
-          <div>
-            <p>Too many matchws, specify another filter</p>
-          </div>
-        )
-      }
-        return (
-          <ul>
-            {countriesToShow.map(country => 
-              <li>{country}</li>
-            )}
-          </ul> 
-        )
-      }
-
-    /**
-    const Country = ({ info }) => {
-        // skip if country is not defined
-        if (country) {
-          console.log('fetching info on country')
-          axios
-            .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${country}`)
-            .then(response => {
-              console.log(response.data)
-              setInfo(response.data)
-            })
-        }
-      return (
-          <li>
-              {info.name}
-          </li>
-      )
-    }  
-    
-    const onSearch = (event) => {
-        event.preventDefault()
-        setCountry(value)
-    }  */
-
-    const Filter = ({ newFilter, handleFilterChange }) => {
+  const Countries = ({ countriesToShow }) => {
+    console.log('countries to show: ',countriesToShow)
+    if (countriesToShow.length > 10) {
       return (
         <div>
-          filter shown with 
-            <input value={newFilter} onChange={handleFilterChange} />
+          <p>Too many matches, specify another filter</p>
         </div>
       )
-    } 
-    const countriesToShow = newFilter 
-      ? countries.filter(country => country.toLowerCase().includes(newFilter.toLowerCase()) === true)
-      : countries
-
-    const handleFilterChange = (event) => {
-        console.log(event.target.value)
-        setNewFilter(event.target.value)
     }
-
-    return (
-    <div>
+    if (countriesToShow.length === 1) {
+      setCountry(countriesToShow[0])
+      console.log('found one country: ', country)
+      return (
       <div>
-        <Filter newFilter={newFilter} handleFilterChange={handleFilterChange} />
+          <Country
+            country={country} 
+            info={info}
+          />
       </div>
-        <Countries countriesToShow={countriesToShow} />
-    </div>
+      )
+    }
+    return (
+      <ul>
+        {countriesToShow.map(country =>
+          <li>{country}</li>
+        )}
+      </ul> 
     )
+  }
+
+  const Country = ({ country, info }) => {
+    console.log(country)
+    const languages = (info.languages)
+    console.log(languages)
+    return (
+      <div>
+        <h2>{country}</h2>
+        <p>Capital {info.capital}</p>
+        <p>Area {info.area}</p>
+        <h2>Languages</h2>
+        <li>languages here</li>
+      </div>
+    )
+  }
+
+  const countriesToShow = value 
+    ? countries.filter(country => country.toLowerCase().includes(value.toLowerCase()) === true)
+    : countries 
+
+  const handleChange = (event) => {
+      console.log(event.target.value)
+      setValue(event.target.value)
+  }
+
+  return (
+    <div>
+      find countries: <input value={value} onChange={handleChange} />
+      <Countries countriesToShow={countriesToShow} />
+    </div>
+  )
 }
 
 export default App
